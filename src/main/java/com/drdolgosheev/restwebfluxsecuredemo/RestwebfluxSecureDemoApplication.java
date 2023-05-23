057b3d23c25fb93527294205566c94058781f9b5
drdolgosheev <*Dima1975>
2023-05-23 13:43:20 +0400

    properties setted up

---
+++ src/main/java/com/drdolgosheev/restwebfluxsecuredemo/RestwebfluxSecureDemoApplication.java
@@ -0,0 +1,13 @@
+package com.drdolgosheev.restwebfluxsecuredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestwebfluxSecureDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestwebfluxSecureDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drdolgosheev/restwebfluxsecuredemo/RestwebfluxsecuredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drdolgosheev.restwebfluxsecuredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestwebfluxsecuredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
